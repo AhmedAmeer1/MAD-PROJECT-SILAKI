50102dc457d8daf0560e97d924116b9d3c875ca9
IT19092102 <ahmedameer1999421@gmail.com>
2020-09-30 07:03:44 +0530

    customer register crud and  and login part is done

---
+++ app/src/main/java/com/example/pharmeasy/Prevalent.java
@@ -0,0 +1,12 @@
+package com.example.pharmeasy;
+
+public class Prevalent {
+
+    public static Register currentOnlineUser;
+
+    public static final String email="email";
+    public static final String password="pass";
+
+
+
+}
