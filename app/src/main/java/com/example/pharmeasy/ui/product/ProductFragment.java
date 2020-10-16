5fd843cfe60f0e29039ff5cac14bfbf707833311
iksudha <iksudhabhargavi2000@gmail.com>
2020-10-17 00:41:01 +0530

    search product

--- app/src/main/java/com/example/pharmeasy/ui/product/ProductFragment.java
+++ app/src/main/java/com/example/pharmeasy/ui/product/ProductFragment.java
@@ -2,11 +2,15 @@ package com.example.pharmeasy.ui.product;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,16 +23,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.pharmeasy.MainActivity;
 import com.example.pharmeasy.R;
 import com.example.pharmeasy.SignIn;
+import com.example.pharmeasy.feedback;
 import com.example.pharmeasy.ui.cart.Cart;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.squareup.picasso.Picasso;
 
 public class ProductFragment extends Fragment {
 
-    private View contactView;
+    private  View contactView;
+    EditText inputSearch;
     private RecyclerView productList;
     private DatabaseReference productreference;
 
@@ -40,10 +47,35 @@ public class ProductFragment extends Fragment {
 
         contactView=inflater.inflate(R.layout.new_product,container,false);
         productList=(RecyclerView)contactView.findViewById(R.id.productList);
+        inputSearch=contactView.findViewById(R.id.searchView);
         productList.setLayoutManager(new GridLayoutManager(getContext(), 2));
         productList.setNestedScrollingEnabled(true);
         productreference = FirebaseDatabase.getInstance().getReference().child("product");
 
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.toString()!=null)
+                {
+                    loadData(s.toString());
+                }
+                else
+                {
+                      loadData("");
+                }
+
+            }
+        });
 
         return contactView;
     }
@@ -51,14 +83,20 @@ public class ProductFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+        loadData("");
+    }
+
+    public void loadData(String data) {
+        Query query=productreference.orderByChild("productName").startAt(data).endAt(data+"\uf8ff");
 
         FirebaseRecyclerOptions<Product> options =
                 new FirebaseRecyclerOptions.Builder<Product>()
-                        .setQuery(productreference, Product.class)
+                        .setQuery(query, Product.class)
                         .build();
         FirebaseRecyclerAdapter<Product, ProductViewHolder> adapter
                 = new FirebaseRecyclerAdapter<Product, ProductViewHolder>(options) {
             @Override
+
             protected void onBindViewHolder(@NonNull ProductViewHolder holder, int i, @NonNull final Product model) {
                 holder.productName.setText(model.getProductName());
                 Picasso.get().load(model.getProductImage()).into(holder.productImage);
@@ -100,12 +138,14 @@ public class ProductFragment extends Fragment {
         TextView productName,price;
         ImageView productImage;
         Button addCart;
+
         public ProductViewHolder(@NonNull View itemView){
             super(itemView);
             addCart= itemView.findViewById(R.id.addtocartbtn);
             productName=itemView.findViewById(R.id.product_name);
             price=itemView.findViewById(R.id.Product_price);
             productImage=itemView.findViewById(R.id.product_image);
+
         }
     }
 }
